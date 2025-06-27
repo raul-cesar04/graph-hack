581abd74d7121cf23b7d819d53e2b4e0874f80ad
raul <185448331+raul-cesar04@users.noreply.github.com>
2025-06-27 12:51:47 -0400

    G1

--- src/main.py
+++ src/main.py
@@ -8,28 +8,37 @@ def main():
 
     g = Graph()
 
-    g.addVertices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+    g.addVertices(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k'])
 
-    g.addAdjacency('a', ('b', 3))
-    g.addAdjacency('a', ('c', 9))
-    g.addAdjacency('a', ('d', 2))
+    g.addAdjacency('a', ('b', 2))
+    g.addAdjacency('a', ('c', 8))
+    g.addAdjacency('a', ('k', 1))
 
-    g.addAdjacency('b', ('c', 4))
-    g.addAdjacency('c', ('d', 6))
+    g.addAdjacency('b', ('c', 6))
+    g.addAdjacency('b', ('d', 1))
+
+    g.addAdjacency('c', ('d', 5))
     g.addAdjacency('c', ('e', 1))
     g.addAdjacency('c', ('f', 2))
-    g.addAdjacency('b', ('e', 5))
-    g.addAdjacency('g', ('d', 9))
-    g.addAdjacency('f', ('g', 1))
-    g.addAdjacency('f', ('h', 5))
-    g.addAdjacency('e', ('h', 5))
-    g.addAdjacency('h', ('j', 5))
-    g.addAdjacency('f', ('j', 9))
-    g.addAdjacency('f', ('i', 6))
-    g.addAdjacency('g', ('i', 2))
-    g.addAdjacency('i', ('j', 3))
+    g.addAdjacency('c', ('k', 7))
 
+    g.addAdjacency('k', ('f', 9))
 
+    g.addAdjacency('f', ('e', 4))
+    g.addAdjacency('f', ('h', 3))
+    g.addAdjacency('f', ('i', 1))
+
+    g.addAdjacency('e', ('h', 6))
+    g.addAdjacency('e', ('d', 3))
+
+    g.addAdjacency('d', ('g', 2))
+    g.addAdjacency('d', ('h', 9))
+
+    g.addAdjacency('g', ('h', 7))
+    g.addAdjacency('g', ('j', 9))
+    g.addAdjacency('h', ('j', 2))
+    g.addAdjacency('h', ('i', 1))
+    g.addAdjacency('i', ('j', 4))
 
     app = App(g)
     
@@ -37,10 +46,10 @@ def main():
     start = g.findVertice('a')
 
     dijkstraResult = Dijkstra.run(g, start)
-    dijkstraResultTable = TableTest(dijkstraResult, 'G2_Dijkstra')
+    dijkstraResultTable = TableTest(dijkstraResult, 'G1_Dijkstra')
 
     primResult = Prim.run(g, start)
-    primResultTable = TableTest(primResult, 'G2_Prim')
+    primResultTable = TableTest(primResult, 'G1_Prim')
 
     app.start()
 
